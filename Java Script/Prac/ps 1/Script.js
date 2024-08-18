d206895b7c67d8bf5cede2a2b704f82856ba3625
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-08-18 14:53:17 +0530

    Java Script Variables

---
+++ Java Script/Prac/ps 1/Script.js
@@ -0,0 +1,20 @@
+let student={
+    name:"Varun",
+    "phone number":9182184378,
+    marks:98
+}
+console.log(student)
+
+let a="Varun";
+a=a+2;
+console.log(a)
+console.log(typeof a)
+
+const obj={
+    name:"Varun",
+    code:5780
+}
+obj.age=20;
+console.log(obj)
+obj.name="Hero";
+console.log(obj)
