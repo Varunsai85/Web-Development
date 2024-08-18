756e9e181a912206cd22778901ee8d520755d3a8
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-08-18 20:56:51 +0530

    Strings

---
+++ Java Script/Prac/ps 4/script.js
@@ -0,0 +1,13 @@
+console.log("Prcatise Set 4")
+//Q1
+console.log("var\"".length)
+//Q2
+let a="Varun Hero"
+let b="Hero"
+console.log(a.includes("r"))
+//Q3
+console.log(a.toLowerCase())
+//Q4
+let s="Please give Rs 1000"
+console.log(s.slice(12))
+console.log(s.replace(s[3],"d"))
---
+++ Java Script/Strings/script.js
@@ -0,0 +1,22 @@
+console.log("Jav Script Strings")
+a="Varun";
+for(let i=0;i<a.length;i++){
+    console.log(a[i]);
+}
+
+//Template Literal
+let real_name="Varun";
+let friend="Manoj"
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "ShivamSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("Sh", "77"))
+console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
+
+console.log(b)
