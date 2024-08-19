f38db7467ffecf2c9d8be46f9c68877d97c8be37
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-08-19 20:01:50 +0530

    Optimised way

--- Java Script/Prac/Factorial/script.js
+++ Java Script/Prac/Factorial/script.js
@@ -1,11 +1,25 @@
 console.log("Factorial of a number")
-let a=prompt("Enter your number")
-function factorial(num){
-    if(num==0 || num==1){
-        return 1
-    }else{
-        num=num*factorial(num-1)
+// let a=prompt("Enter your number")
+// function factorial(num){
+//     if(num==0 || num==1){
+//         return 1
+//     }else{
+//         num=num*factorial(num-1)
+//     }
+//     return num
+// }
+// console.log("The factorial of the given number is",factorial(a))
+// The Above method is also correct
+let a = 5
+function factorial(number) {
+    if (number == 0) {
+        console.log(1)
+    } else {
+        let arr = Array.from(Array(number + 1).keys())
+        arr.shift()
+        console.log(arr.reduce((a, b) => {
+            return a * b;
+        }))
     }
-    return num
 }
-console.log("The factorial of the given number is",factorial(a))
+factorial(a)
