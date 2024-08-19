ef03df7f68c8b488af75b8f43e5fe593b0949359
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-08-19 19:19:04 +0530

    Practise set 7

---
+++ Java Script/Prac/ps 7/script.js
@@ -0,0 +1,12 @@
+console.log("Prcatise set 7")
+//Q1
+document.querySelector("li").style.color="red"
+
+//Q2
+// document.querySelector("ul").firstElementChild.style.backgroundColor="green"
+// document.querySelector("ul").lastElementChild.style.backgroundColor="green"
+
+//Q3
+document.querySelectorAll("li").forEach(e=>{
+    e.style.backgroundColor="cyan"
+})
