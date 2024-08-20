ff85a8587471a80102bc4d27bf011e50e1977cc0
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-08-20 15:01:03 +0530

    JS console methods

---
+++ Java Script/Prac/ps 6/script.js
@@ -0,0 +1,24 @@
+console.log("Practise set 6")
+//Q1 Q2 Q3
+
+/*let runAgain=true
+while (runAgain) {
+    let age = prompt("Enter your age")
+    age = parseInt(age)
+    if (age >= 18) {
+        alert("You can drive")
+    } else if (age <= 0) {
+        console.error("Enter the valid age")
+        break
+    }
+    else {
+        alert("You cannot drive")
+    }
+    runAgain = confirm("Repeat the question?")
+}*/
+
+//Q4
+let number=prompt("Enter your number")
+if(number>4){
+    location.href="https://www.google.co.in"
+}
