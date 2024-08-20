2df04c7b1daea7ff3f48ddf5cc820ca4c302c643
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-08-20 23:17:03 +0530

    trails

---
+++ Java Script/script.js
@@ -0,0 +1,77 @@
+console.log("Oorike Saradha!!!")
+
+const obj={
+    name:"Varun",
+    age:20
+}
+
+// for(keys in obj){
+//     if(Object.hasOwnProperty.call(obj,keys)){
+//         const element=obj[keys]
+//         console.log(keys,element)
+//     }
+// }
+
+// console.log("I\'am very good boy said by my \"My Mother\"")
+// let a="Varun"
+// console.log(`I'am "${a}"`)
+// let name1="Varun"
+// let name2="sai"
+// String are Immutable
+// console.log(name1.concat(" ",name2))
+// console.log(name1)
+
+let arr=[2,3,4,1]
+// console.log(arr.toString())
+// console.log(arr)
+// arr.splice(1,2,56,78)
+// console.log(arr)
+// arr.sort()
+// console.log(arr)
+// arr.reverse()
+// console.log(arr)
+// arr.forEach((a,b,c)=>{
+//     console.log(b,a)
+// })
+// let maparr=arr.map((a,b,c)=>{
+//     return a*b
+// })
+// console.log(maparr)
+
+// console.log(arr.filter(greater_than_2=(a,b,c)=>{
+//     if(a>2) return true
+//     return false
+// }))
+// function factorial(num){
+//     let arr=Array.from(Array(num+1).keys())
+//     arr.shift()
+//     console.log(arr.reduce((a,b)=>{
+//         return a*b
+//     }))
+// }
+// factorial(5)
+// for(keys in arr){
+//     if(Object.hasOwnProperty.call(arr,keys)){
+//         const element=arr[keys]
+//         console.log(element)
+//     }
+// }
+// for(i of arr){
+//     console.log(i)
+// }
+
+// const object={
+//     id:123,
+//     name:"Varun",
+//     age:20,
+//     number:9182184378
+// }
+// for(keys in object){
+//     if(Object.hasOwnProperty.call(object,keys)){
+//         const element=object[keys]
+//         console.log(keys,element)
+//     }
+// }
+arr.forEach(e=>{
+    console.log(e)
+})
