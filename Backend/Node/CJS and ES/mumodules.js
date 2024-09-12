fda6bb817de2b4b2fa597debe055993116f5cabe
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-09-12 18:44:58 +0530

    Importing and types of JS

---
+++ Backend/Node/CJS and ES/mumodules.js
@@ -0,0 +1,12 @@
+const obj1={
+    a:1,
+    b:2,
+    c:4
+};
+export default obj1;
+
+export const obj2={
+    x:22,
+    y:44,
+    z:55
+}
---
+++ Backend/Node/CJS and ES/mymodule2.js
@@ -0,0 +1,8 @@
+const square=(x)=>{
+    return x*x;
+}
+const cube=(y)=>{
+    return y*y*y
+}
+module.exports={square,cube}
+
