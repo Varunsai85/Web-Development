cd0a057b875f91c4e12287aaf14c3c8e3f304f8b
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-10-13 22:51:50 +0530

    Modified reision

--- Backend/Node/Practise/fs modules/fspromises.js
+++ Backend/Node/Practise/fs modules/fspromises.js
@@ -1,8 +1,10 @@
-import fs from "fs/promises";
+const fs = require("fs/promises");
 
+(async function file() {
 
-let b=await fs.appendFile("varun.txt"," this append text for varun.txt");
+    let b = await fs.appendFile("varun.txt", " this append text for varun.txt");
 
-let a=await fs.readFile("varun.txt");
+    let a =await fs.readFile("varun.txt");
 
-console.log(a.toString());
+    console.log(a.toString());
+})();
