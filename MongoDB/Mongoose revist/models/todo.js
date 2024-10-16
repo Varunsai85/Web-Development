41f6aa7e738a8fbb912806acd09f0d90f8384f48
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-10-16 23:06:53 +0530

    Schema Object

--- MongoDB/Mongoose revist/models/todo.js
+++ MongoDB/Mongoose revist/models/todo.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 
 const TodoSchema=new mongoose.Schema({
     title:String,
+    //title:{type:String , required:true, default:"Hey"} -> we can give the schema as an object to the attribute
     desc:String,
     isDone:Boolean,
     days:Number
