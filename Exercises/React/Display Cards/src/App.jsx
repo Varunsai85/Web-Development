d2e7bd263338237ac7b63ff7313ea0010c8459a5
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-10-19 02:13:10 +0530

    update

--- Exercises/React/Display Cards/src/App.jsx
+++ Exercises/React/Display Cards/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const [json, setJson] = useState([])
-  const fetchData=async ()=>{
+  const fetchData = async () => {
     let a = await fetch('https://jsonplaceholder.typicode.com/posts')
     let response = await a.json();
     setJson(response)
@@ -14,24 +14,26 @@ function App() {
     fetchData()
   }, [])
 
-  const Card=(props)=>{
+  const Card = (props) => {
     return (
-    <div className="border-black border-2 max-w-56">
-      <div className="datainside">userId : {props.userId}</div>
-      <div className="datainside">id : {props.id}</div>
-      <div className="datainside">title : {props.title}</div>
-      <div className="datainside">body : {props.body}</div>
-    </div>
+      <>
+        <div className="border-white border-2 max-w-56">
+          <div className="datainside">userId : {props.userId}</div>
+          <div className="datainside">id : {props.id}</div>
+          <div className="datainside">title : {props.title}</div>
+          <div className="datainside">body : {props.body}</div>
+        </div>
+      </>
     )
-}
+  }
 
   return (
     <>
-    <div className="main-container flex gap-y-6 flex-wrap justify-between">
-      {json.map((resp)=>{
-        return <Card key={resp.id} userId={resp.userId} id={resp.id} title={resp.title} body={resp.body}/>
-      })}
-    </div>
+      <div className="main-container flex gap-y-6 flex-wrap justify-between m-2">
+        {json.map((resp) => {
+          return <Card key={resp.id} userId={resp.userId} id={resp.id} title={resp.title} body={resp.body} />
+        })}
+      </div>
     </>
   )
 }
