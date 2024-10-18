f2f77d42a2a4c54529488f8e90c947a13d5a0a2e
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-10-18 23:14:27 +0530

    Displaying the cards

---
+++ Exercises/React/Display Cards/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [json, setJson] = useState([])
+  const fetchData=async ()=>{
+    let a = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let response = await a.json();
+    setJson(response)
+    console.log(response);
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const Card=(props)=>{
+    return (
+    <div className="border-black border-2 max-w-56">
+      <div className="datainside">userId : {props.userId}</div>
+      <div className="datainside">id : {props.id}</div>
+      <div className="datainside">title : {props.title}</div>
+      <div className="datainside">body : {props.body}</div>
+    </div>
+    )
+}
+
+  return (
+    <>
+    <div className="main-container flex gap-y-6 flex-wrap justify-between">
+      {json.map((resp)=>{
+        return <Card key={resp.id} userId={resp.userId} id={resp.id} title={resp.title} body={resp.body}/>
+      })}
+    </div>
+    </>
+  )
+}
+
+export default App
