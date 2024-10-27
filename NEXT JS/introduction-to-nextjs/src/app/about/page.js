ece63cfb8d820210208ae689c6203c363c46dc19
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2024-10-27 15:12:36 +0530

    Intro to NextJs

---
+++ NEXT JS/introduction-to-nextjs/src/app/about/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "@/components/Navbar"
+
+const about = () => {
+  return (
+    <>
+    <Navbar/>
+    <div>
+      Hi this is about
+    </div>
+    </>
+  )
+}
+
+export default about
---
+++ NEXT JS/introduction-to-nextjs/src/app/contact-us/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+
+const contact = () => {
+    return (
+        <>
+            <Navbar />
+            <div>
+            contact US
+            </div>
+        </>
+    )
+}
+
+export default contact
